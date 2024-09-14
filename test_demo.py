e62179ca3bab3a51d5ef1de5ef3f215b67773bba
shaozhb <275803332@163.com>
2024-09-14 10:44:07 +0800

    succ

--- test_demo.py
+++ test_demo.py
@@ -5,9 +5,13 @@ def test_example():
     assert 1 == 1
 
 
+def test_example1():
+    assert 1 == 1
+
+
 def test_failure():
     assert 3 == 3
 
 
 if __name__ == '__main__':
-    pytest.main(["-s", "-v", "--html=./allure-report", "--alluredir=./allure-results"])
+    pytest.main(["-s", "-v", "--html=./allure-report/pytest.html", "--alluredir=./allure-results"])
